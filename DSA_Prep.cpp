a782dc57e6b2b96cd3a74859651186aa9693ecd5
sarankumar k <sarankumark@corp.bharatpetroleum.com>
2021-10-08 22:14:39 +0530

    Added sort linked list having 0,1,2

--- DSA_Prep.cpp
+++ DSA_Prep.cpp
@@ -22,20 +22,25 @@ int main()
      head = insertAtEnd_LL(head, 30);
      head = insertAtEnd_LL(head, 40);*/
 
-     head = insertAtGivenPos_LL(nullptr, 10, 1);
-     head = insertAtGivenPos_LL(head, 20, 2);
-     head = insertAtGivenPos_LL(head, 30, 1);
-     head = insertAtGivenPos_LL(head, 40,4 );
-     head = insertAtGivenPos_LL(head, 50, 6);
-     head = insertAtGivenPos_LL(head, 100, 2);
+     head = insertAtGivenPos_LL(nullptr, 1, 1);
+     head = insertAtGivenPos_LL(head, 2, 2);
+     head = insertAtGivenPos_LL(head, 0, 3);
+     head = insertAtGivenPos_LL(head, 0,4 );
+     head = insertAtGivenPos_LL(head, 1, 5);
+     head = insertAtGivenPos_LL(head, 0, 6);
 
     //traverse_LL(head);
-     recursiveTraversal_LL(head);
+    /* recursiveTraversal_LL(head);*/
      //cout << search_LL(head, 100) << endl;
-     cout << recursiveSearch_LL(head, 20,1)<<endl;
-     cout << recursiveSearch2_LL(head, 400)<<endl;
- //--------
-
+    /* cout << recursiveSearch_LL(head, 20,1)<<endl;
+     cout << recursiveSearch2_LL(head, 400)<<endl;*/
+   
+    /* head = sortedInsert_LL(head, 45);*/
+     recursiveTraversal_LL(head);
+    /* sort012ByChangingData_LL(head);*/
+     head = sort012ByChangingLink_LL(head);
+     cout << endl;
+     recursiveTraversal_LL(head);
 
 }
 
--- LinkedList.cpp
+++ LinkedList.cpp
@@ -148,3 +148,127 @@ int recursiveSearch2_LL(Node* head, int searchData)
 	}
 
 }
+
+Node* sortedInsert_LL(Node* head, int dataToInsert)
+{
+	Node* temp = new Node(dataToInsert);
+
+	if (head == nullptr)
+	{   
+		return temp;
+	}
+
+	if (head->data > dataToInsert)
+	{
+		temp->next = head;
+		return temp;
+	}
+
+
+	Node* curr = head;
+
+	while (curr->next != nullptr && curr->next->data < dataToInsert)
+	{
+		curr = curr->next;
+	}
+
+	temp->next = curr->next;
+	curr->next = temp;
+
+	return head;
+}
+
+void sort012ByChangingData_LL(Node* head)
+{
+	if (head == nullptr)
+		return;
+
+	int zero_count = 0, one_count = 0, two_count = 0;
+
+	Node* curr1 = head;
+
+	while (curr1 != nullptr)
+	{
+		if (curr1->data == 0) zero_count++;
+		else if (curr1->data == 1) one_count++;
+		else two_count++;
+
+		curr1 = curr1->next;
+	}
+
+	Node* curr = head;
+	
+		for (int i = zero_count; i > 0; i--)
+		{
+			curr->data = 0;
+			curr = curr->next;
+		}
+
+		for (int i = one_count; i > 0; i--)
+		{
+			curr->data = 1;
+			curr = curr->next;
+		}
+
+		for (int i = two_count; i > 0; i--)
+		{
+			curr->data = 2;
+			curr = curr->next;
+		}
+	
+
+}
+
+Node* sort012ByChangingLink_LL(Node* head)
+{
+	if (head == nullptr || head->next == nullptr) // if LL is null or having one node
+		return head;
+
+	int zero_count = 0, one_count = 0,two_count = 0;
+
+	Node* zero_head = new Node(0); //dummy nodes
+	Node* curr_zero = zero_head;
+
+	Node* one_head = new Node(0); //dummy nodes
+	Node* curr_one = one_head;
+
+	Node* two_head = new Node(0); //dummy nodes
+	Node* curr_two = two_head;
+
+	Node* curr = head;
+
+	while (curr != nullptr)
+	{
+		if (curr->data == 0)
+		{
+			curr_zero->next = curr;
+			curr_zero = curr_zero->next;
+			zero_count++;
+		}	
+		else if (curr->data == 1)
+		{
+			curr_one->next = curr;
+			curr_one = curr_one->next;
+			one_count++;
+		}			
+		else
+		{
+			curr_two->next = curr;
+			curr_two = curr_two->next;
+			two_count++;
+		}
+			
+
+		curr = curr->next;
+	}
+
+
+	curr_zero->next = (one_head->next) ? (one_head->next) : (two_head->next);
+	curr_one->next = two_head->next;
+	curr_two->next = NULL;
+
+	// Updated head
+	head = zero_head->next;
+
+	return head;
+}
